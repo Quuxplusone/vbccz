8dd625cf6159377a5c8f72f8797c663b4b2d91e0
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-04-12 21:29:20 -0700

    Add my stub implementation of the standard library.
    In particular, "assert" will allow me to write regression tests
    for the backend bugs that I fix.

---
+++ z-machine/include/limits.h
@@ -0,0 +1,24 @@
+#ifndef H_LIMITS
+ #define H_LIMITS
+
+#define CHAR_BIT 8
+
+#define CHAR_MIN 0
+#define CHAR_MAX 127
+#define SCHAR_MIN (-128)
+#define SCHAR_MAX 127
+#define UCHAR_MAX 255
+
+#define SHRT_MIN (-32767 - 1)
+#define SHRT_MAX 32767
+#define USHRT_MAX 65535U
+
+#define INT_MIN (-32767 - 1)
+#define INT_MAX 32767
+#define UINT_MAX 65535U
+
+#define LONG_MIN (-2147483647L - 1)
+#define LONG_MAX (2147483647L)
+#define ULONG_MAX (4294967295UL)
+
+#endif /* H_LIMITS */
