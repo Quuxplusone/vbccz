96e1b17f136a5c8991905f63792a853b73667289
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-11-17 16:22:21 -0800

    Fix an ICE and implement "char -> long" conversions.
    Type conversions from CHAR to LONG were implemented in the compiler but
    not in "runtime.inf"; the `__long_fromchar` routine was missing.
    
    Type conversions from CHAR to UNSIGNED|LONG were not implemented in
    the compiler, due to an oversight in a switch statement; this resulted
    in an ICE.
    
    Both of these bugs are now fixed.

--- test/conversions.c
+++ test/conversions.c
@@ -11,8 +11,8 @@ unsigned int sc_ui(signed char c) { return c; }
 int uc_si(unsigned char c) { return c; }
 unsigned int uc_ui(unsigned char c) { return c; }
 
-//long sc_sl(signed char c) { return c; }
-//unsigned long sc_ul(signed char c) { return c; }
+long sc_sl(signed char c) { return c; }
+unsigned long sc_ul(signed char c) { return c; }
 long uc_sl(unsigned char c) { return c; }
 unsigned long uc_ul(unsigned char c) { return c; }
 
@@ -44,8 +44,8 @@ int main()
     assert(sc_ui(42) == 42u);
     assert(uc_si(42) == 42);
     assert(uc_ui(42) == 42u);
-    //assert(sc_sl(42) == 42L);
-    //assert(sc_ul(42) == 42uL);
+    assert(sc_sl(42) == 42L);
+    assert(sc_ul(42) == 42uL);
     assert(uc_sl(42) == 42L);
     assert(uc_ul(42) == 42uL);
     assert(ss_si(42) == 42);
@@ -69,8 +69,8 @@ int main()
     assert(sc_ui(-42) == 0u - 42u);
     assert(uc_si(-42) == 256 - 42);
     assert(uc_ui(-42) == 256 - 42);
-    //assert(sc_sl(-42) == -42L);
-    //assert(sc_ul(-42) == 0uL - 42uL);
+    assert(sc_sl(-42) == -42L);
+    assert(sc_ul(-42) == 0uL - 42uL);
     assert(uc_sl(-42) == 256 - 42);
     assert(uc_ul(-42) == 256 - 42);
     assert(ss_si(-42) == -42);
