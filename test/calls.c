842024bf91b1e5e46788d98de680e41dcdbde9c6
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-11-29 22:22:22 -0800

    Fix wrong codegen in dangerous_IC() and a segfault in dump_obj().
    The sense of dangerous_IC()'s return value had been flipped for some reason:
    the compiler was actually trying to hoist divisions and dereferences whenever
    possible, and *failing* to hoist all other operations. This leads to inefficient
    codegen in general, but also to incorrectly hoisted divide-by-zeroes.
    Even an expression as simple as `(d ? 1/d : 0)` would do the wrong thing,
    if placed inside a loop at `-O2`.
    
    The test case in this patch exposes the bug at `-O2` but not at `-O3`.
    At `-O2`, it consistently generates a hoisted division by zero, which
    corresponds to "fatal error: divide by zero" in the Bocfel interpreter.
    
    Furthermore, when given the IC for `*(int*)0`, dump_obj() got confused
    by the IC's `flags==(KONST|DREFOBJ)`. The old dump_obj() assumed that the
    only time `(flags&KONST)` is when `(flags==KONST)`, so it would fall into
    the codepath that assumed `(flags&VAR)`, dereference `obj->var` which would
    be garbage, and segfault. This is now fixed.

--- test/calls.c
+++ test/calls.c
@@ -25,9 +25,28 @@ void variadic_function(int a, ...)
     assert(c == 3);
 }
 
+void test_dangerous_ops(int *a, int n, int k, int *p)
+{
+    for (int i=0; i < n; ++i) {
+        a[6*i+0] = (k ? 1 / k : i);
+        a[6*i+1] = (k ? 1 % k : i);
+        a[6*i+2] = (k ? *p : i);
+        a[6*i+3] = (k ? 1 / 0 : i);
+        a[6*i+4] = (k ? 1 % 0 : i);
+        a[6*i+5] = (k ? *(int *)0 : i);
+    }
+}
+
 int main()
 {
     regular_function(a(), b(2), c());
     variadic_function(a(), b(2), c());
+    {
+        int arr[12];
+        test_dangerous_ops(arr, 2, 0, NULL);
+        for (int i=0; i < 12; ++i) {
+            assert(arr[i] == i/6);
+        }
+    }
     puts("SUCCESS!");
 }
