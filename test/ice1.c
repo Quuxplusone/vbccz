5c9a0ef5cdedce612658a9a720ecb68f1e9e184c
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-12-04 00:02:28 -0800

    Add a test file that causes an ICE in declaration.c.
    This is a syntax error, but it would be nice if it didn't ICE the compiler.

---
+++ test/ice1.c
@@ -0,0 +1,10 @@
+int x() int y(){}
+/*
+>int x() int y(){
+error 37 in line 1 of "ice1.c": <y> is no parameter
+>int x() int y(){
+warning 54 in line 1 of "ice1.c": ; expected
+warning 173 in function "x": function <x> has no return statement
+>EOF
+error 158 in line 11 of "ice1.c": internal error 0 in line 2936 of file declaration.c !!
+*/
