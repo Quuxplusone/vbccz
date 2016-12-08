f600ad0e4ff90f2e3c4b7301013fbd13427c013f
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2016-12-08 00:53:54 -0800

    Fix an ICE where `nocode` gets out of sync with reality.
    A label or a switch-case can reset `nocode` from 1 to 0, which can cause
    the `BRA` at the end of an `if` block to get generated... but by the time
    if_statement() gets around to generating the corresponding `LABEL`,
    it will have re-reset `nocode=cm`. This causes us to generate a branch
    to an undefined label, anytime an `if` containing a label or switch-case
    appears in a context that is `nocode` for some other reason. This bug
    is now fixed.

--- test/ice.c
+++ test/ice.c
@@ -13,9 +13,24 @@ void ice1(int x)
     x = (0 / (a[0][0] + (i=3))) >> x; assert(i == 3); assert(x == 0);
 }
 
+void ice2(int x)
+{
+    if (0) {
+        if (0) { L1:; } else {}
+    }
+    if (0) {
+        if (x) { L2:; } else {}
+    }
+    if (0) {
+        if (1) { L3:; } else {}
+    }
+}
+
 int main()
 {
     struct S s; (s.f = 42) + 0; assert(s.f == 42);
     ice1(0);
+    ice2(0);
+    ice2(1);
     puts("SUCCESS!");
 }
