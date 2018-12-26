9bc1164d597349f9132f3e1f4964668f8909b220
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2018-12-25 21:10:52 -0500

    ff

---
+++ test/printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("TEST 1  single > '    <");
+    puts("TEST 2  double > \"    <");
+    puts("TEST 3  smart  > ”    <");
+    puts("TEST 4  caret  > ^    <");
+    puts("TEST 5  tilde  > ~    <");
+    puts("TEST 6  at     > @    <");
+    puts("TEST 7  back   > \\    <");
+    puts("TEST 8  slash  > /    <");
+    puts("TEST 9  curly  > {    <");
+    puts("TEST 10 curly  > }    <");
+    puts("TEST 11 caret0 > ^0   <");
+    puts("TEST 12 tilde0 > ~0   <");
+    puts("TEST 13 @caret > @^   <");
+    puts("TEST 14 @tilde > @~   <");
+    puts("TEST 15 atat94 > @@94 <");
+    puts("SUCCESS!");
+}
