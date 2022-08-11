30c6ae2bc128e3a8a646fcd297c8e3c1e2bff315
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-08-11 11:51:36 -0400

    [test] Add coverage for read_reg() line 1164

--- test/struct.c
+++ test/struct.c
@@ -92,11 +92,57 @@ void test_variadics()
     assert(varb(b, 42) == 48);
 }
 
+struct SC { char m; };
+struct SS { short m; };
+struct SI { int m; };
+struct SL { long m; };
+int add_c_sc(char a, struct SC s) { return a + s.m; }
+int add_c_ss(char a, struct SS s) { return a + s.m; }
+int add_c_si(char a, struct SI s) { return a + s.m; }
+long add_c_sl(char a, struct SL s) { return a + s.m; }
+int add_s_sc(short a, struct SC s) { return a + s.m; }
+int add_s_ss(short a, struct SS s) { return a + s.m; }
+int add_s_si(short a, struct SI s) { return a + s.m; }
+long add_s_sl(short a, struct SL s) { return a + s.m; }
+int add_i_sc(int a, struct SC s) { return a + s.m; }
+int add_i_ss(int a, struct SS s) { return a + s.m; }
+int add_i_si(int a, struct SI s) { return a + s.m; }
+long add_i_sl(int a, struct SL s) { return a + s.m; }
+long add_l_sc(long a, struct SC s) { return a + s.m; }
+long add_l_ss(long a, struct SS s) { return a + s.m; }
+long add_l_si(long a, struct SI s) { return a + s.m; }
+long add_l_sl(long a, struct SL s) { return a + s.m; }
+
+void test_different_types()
+{
+    struct SC sc = { 0x01 };
+    struct SS ss = { 0x0102 };
+    struct SI si = { 0x0102 };
+    struct SL sl = { 0x01020304 };
+    assert(add_c_sc(0x10, sc) == 0x11);
+    assert(add_c_ss(0x10, ss) == 0x0112);
+    assert(add_c_si(0x10, si) == 0x0112);
+    assert(add_c_sl(0x10, sl) == 0x01020314);
+    assert(add_s_sc(0x1020, sc) == 0x1021);
+    assert(add_s_ss(0x1020, ss) == 0x1122);
+    assert(add_s_si(0x1020, si) == 0x1122);
+    assert(add_s_sl(0x1020, sl) == 0x01021324);
+    assert(add_i_sc(0x1020, sc) == 0x1021);
+    assert(add_i_ss(0x1020, ss) == 0x1122);
+    assert(add_i_si(0x1020, si) == 0x1122);
+    assert(add_i_sl(0x1020, sl) == 0x01021324);
+    assert(add_l_sc(0x10203040, sc) == 0x10203041);
+    assert(add_l_ss(0x10203040, ss) == 0x10203142);
+    assert(add_l_si(0x10203040, si) == 0x10203142);
+    assert(add_l_sl(0x10203040, sl) == 0x11223344);
+}
+
 int main()
 {
     test_small_struct();
     test_large_struct();
     test_variadics();
+    test_different_types();
 
     puts("SUCCESS!");
 }
